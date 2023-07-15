ce5b0144678c90cc04edc5c12a3c7e8789984a18
Dipanshu ghime <dipanshughime@gmail.com>
2023-07-15 20:18:28 +0530

    hehe

--- client/src/pages/gpt.js
+++ client/src/pages/gpt.js
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import { useState } from 'react';
 function Gpt() {
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState( [] ) ;
+
+
+    const handlerSend = () => {
+        console.log('send');
+    }
   return (
-    <div>gpt</div>
+<div className='App'>
+    <div className='chat'>
+    <div className='user-message'>this is User msg</div>
+    <div className='bot-message'>this is bot msg</div>
+    </div>
+
+    <div className=" input-container">
+<input
+type="text" value={input}
+onChange={(e) => setInput (e.target. value)}
+/>
+<button  className="button" onClick={handlerSend}>send</button>
+</div>
+
+</div>
   )
 }
 
