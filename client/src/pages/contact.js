c1df1433b9732c12ba7af3b656a5547c1275b823
Dipanshu ghime <dipanshughime@gmail.com>
2023-07-16 20:40:57 +0530

    about us and contact us created

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -2,7 +2,53 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>contact</div>
+    <div className="bg-gray-100 min-h-screen">
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold text-center mb-8">Contact Us</h1>
+      <div className="max-w-4xl mx-auto bg-white p-8 shadow-md">
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              className="w-full px-3 py-2 border rounded-lg text-gray-700 h-32 resize-none focus:outline-none focus:border-blue-500"
+              id="message"
+              placeholder="Enter your message"
+            ></textarea>
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
   )
 }
 
